e0fe8bc0454bb17a2bccab37a0c408253f828d18
VLADISLAV <SIZONOV.95@MAIL.RU>
2023-11-25 17:30:32 +0300

    added swiftgen

---
+++ Cofio/Resources/Generated/Strings+Generated.swift
@@ -0,0 +1,212 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+  /// Чтобы добавить карточку нажите плюс вверху экрана
+  internal static let cardsModuleEmptyCellSubtitle = L10n.tr("Localizable", "cards_module_empty_cell_subtitle", fallback: "Чтобы добавить карточку нажите плюс вверху экрана")
+  /// Нет карточек
+  internal static let cardsModuleEmptyCellTitle = L10n.tr("Localizable", "cards_module_empty_cell_title", fallback: "Нет карточек")
+  /// Карточки
+  internal static let cardsModuleTitle = L10n.tr("Localizable", "cards_module_title", fallback: "Карточки")
+  /// По этой теме пока не добавлены карточки.
+  ///  Добавьте их в главном разделе
+  internal static let cardsRepetitionModuleEmptyCellTitle = L10n.tr("Localizable", "cards_repetition_module_empty_cell_title", fallback: "По этой теме пока не добавлены карточки.\n Добавьте их в главном разделе")
+  /// Выучил
+  internal static let cardsRepetitionModuleLearnedButton = L10n.tr("Localizable", "cards_repetition_module_learned_button", fallback: "Выучил")
+  /// Еще раз
+  internal static let cardsRepetitionModuleMoreButton = L10n.tr("Localizable", "cards_repetition_module_more_button", fallback: "Еще раз")
+  /// Localizable.strings
+  ///   Cofio
+  /// 
+  ///   Created by Владислав Сизонов on 14.12.2022.
+  internal static let closeButtonTitle = L10n.tr("Localizable", "close_button_title", fallback: "Закрыть")
+  /// Название коллекции
+  internal static let collectionTextFieldPlaceholder = L10n.tr("Localizable", "collection_textField_placeholder", fallback: "Название коллекции")
+  /// Удалить
+  internal static let deleteButtonTitle = L10n.tr("Localizable", "delete_button_title", fallback: "Удалить")
+  /// Понятно
+  internal static let finishOfferModuleButtonTitle = L10n.tr("Localizable", "finish_offer_module_button_title", fallback: "Понятно")
+  /// выучена!
+  internal static let finishOfferModuleLearned = L10n.tr("Localizable", "finish_offer_module_learned", fallback: "выучена!")
+  /// Следующее повторение
+  internal static let finishOfferModuleNextRepeat = L10n.tr("Localizable", "finish_offer_module_next_repeat", fallback: "Следующее повторение")
+  /// повторена
+  internal static let finishOfferModuleRepeated = L10n.tr("Localizable", "finish_offer_module_repeated", fallback: "повторена")
+  /// Тема
+  internal static let finishOfferModuleTitleLabel = L10n.tr("Localizable", "finish_offer_module_title_label", fallback: "Тема")
+  /// Для запоминания используется методика прогрессирующего запоминания, первое повторение происходит в тот же день, далее на следующий, потом через 3 дня, 5....
+  internal static let learningMethodModuleLongCellSubtitle = L10n.tr("Localizable", "learning_method_module_long_cell_subtitle", fallback: "Для запоминания используется методика прогрессирующего запоминания, первое повторение происходит в тот же день, далее на следующий, потом через 3 дня, 5....")
+  /// Прогрессирующее повторение
+  internal static let learningMethodModuleLongCellTitle = L10n.tr("Localizable", "learning_method_module_long_cell_title", fallback: "Прогрессирующее повторение")
+  /// Выбор способа запоминания
+  internal static let learningMethodModuleTitle = L10n.tr("Localizable", "learning_method_module_title", fallback: "Выбор способа запоминания")
+  /// Методика состоит в том, чтобы повторять информацию каждый день в течение недели
+  internal static let learningMethodModuleWeekCellSubtitle = L10n.tr("Localizable", "learning_method_module_week_cell_subtitle", fallback: "Методика состоит в том, чтобы повторять информацию каждый день в течение недели")
+  /// Каждодневное повторение
+  internal static let learningMethodModuleWeekCellTitle = L10n.tr("Localizable", "learning_method_module_week_cell_title", fallback: "Каждодневное повторение")
+  /// Чтобы добавить коллекцию нажите плюс внизу экрана
+  internal static let mainModuleEmptyCellSubtitle = L10n.tr("Localizable", "main_module_empty_cell_subtitle", fallback: "Чтобы добавить коллекцию нажите плюс внизу экрана")
+  /// Нет коллекций
+  internal static let mainModuleEmptyCellTitle = L10n.tr("Localizable", "main_module_empty_cell_title", fallback: "Нет коллекций")
+  /// Перейти в настройки
+  internal static let mainModuleLearningMethodAlertButtonTitle = L10n.tr("Localizable", "main_module_learning_method_alert_button_title", fallback: "Перейти в настройки")
+  /// Перейдите в вкладку Настройки и выберите способ запоминания информации
+  internal static let mainModuleLearningMethodAlertMessage = L10n.tr("Localizable", "main_module_learning_method_alert_message", fallback: "Перейдите в вкладку Настройки и выберите способ запоминания информации")
+  /// Не выбран способ запоминания
+  internal static let mainModuleLearningMethodAlertTitle = L10n.tr("Localizable", "main_module_learning_method_alert_title", fallback: "Не выбран способ запоминания")
+  /// Нет тем для повторения
+  internal static let mainModuleRepeatNoThemesToastTitle = L10n.tr("Localizable", "main_module_repeat_no_themes_toast_title", fallback: "Нет тем для повторения")
+  /// Повторение коллекции началось
+  internal static let mainModuleRepeatStartedToastTitle = L10n.tr("Localizable", "main_module_repeat_started_toast_title", fallback: "Повторение коллекции началось")
+  /// Изучить коллекцию
+  internal static let mainModuleStartLearningTitle = L10n.tr("Localizable", "main_module_start_learning_title", fallback: "Изучить коллекцию")
+  /// Карточки для запоминания любой информации, подготовки к экзаменам, изучения иностранных слов
+  internal static let mainModuleSubtitle = L10n.tr("Localizable", "main_module_subtitle", fallback: "Карточки для запоминания любой информации, подготовки к экзаменам, изучения иностранных слов")
+  /// Главная
+  internal static let mainModuleTitle = L10n.tr("Localizable", "main_module_title", fallback: "Главная")
+  /// Главная
+  internal static let mainTab = L10n.tr("Localizable", "main_tab", fallback: "Главная")
+  /// Добавить карточку
+  internal static let newCardModuleAddButtonTitle = L10n.tr("Localizable", "new_card_module_add_button_title", fallback: "Добавить карточку")
+  /// Определение или слово
+  internal static let newCardModuleAddDefinitionTextViewPlaceholder = L10n.tr("Localizable", "new_card_module_add_definition_textView_placeholder", fallback: "Определение или слово")
+  /// Описание или перевод
+  internal static let newCardModuleAddDescriptionTextViewPlaceholder = L10n.tr("Localizable", "new_card_module_add_description_textView_placeholder", fallback: "Описание или перевод")
+  /// Редактировать
+  internal static let newCardModuleEditButtonTitle = L10n.tr("Localizable", "new_card_module_edit_button_title", fallback: "Редактировать")
+  /// Редактировать
+  internal static let newCollectionModuleButtonEditTitle = L10n.tr("Localizable", "new_collection_module_button_edit_title", fallback: "Редактировать")
+  /// Добавить
+  internal static let newCollectionModuleButtonTitle = L10n.tr("Localizable", "new_collection_module_button_title", fallback: "Добавить")
+  /// Выберите иконку
+  internal static let newCollectionModuleChooseIcon = L10n.tr("Localizable", "new_collection_module_choose_icon", fallback: "Выберите иконку")
+  /// Редактирование коллекции
+  internal static let newCollectionModuleEditTitle = L10n.tr("Localizable", "new_collection_module_edit_title", fallback: "Редактирование коллекции")
+  /// Добавление коллекции
+  internal static let newCollectionModuleTitle = L10n.tr("Localizable", "new_collection_module_title", fallback: "Добавление коллекции")
+  /// Дальше
+  internal static let onboardingModuleButtonTitle = L10n.tr("Localizable", "onboarding_module_button_title", fallback: "Дальше")
+  /// Изучайте и запоминайте информацию с помощью карточек.
+  internal static let onboardingModuleSubtitle1 = L10n.tr("Localizable", "onboarding_module_subtitle1", fallback: "Изучайте и запоминайте информацию с помощью карточек.")
+  /// Для удобства все темы можно группировать в коллекции. Для каждой коллекции выбирается своя иконка.
+  internal static let onboardingModuleSubtitle2 = L10n.tr("Localizable", "onboarding_module_subtitle2", fallback: "Для удобства все темы можно группировать в коллекции. Для каждой коллекции выбирается своя иконка.")
+  /// Рядом с названием каждой темы есть прогресс, показываеющий сколько повторений темы сделано.
+  internal static let onboardingModuleSubtitle3 = L10n.tr("Localizable", "onboarding_module_subtitle3", fallback: "Рядом с названием каждой темы есть прогресс, показываеющий сколько повторений темы сделано.")
+  /// В каждую тему можно добавить неограниченное количество карточек.
+  internal static let onboardingModuleSubtitle4 = L10n.tr("Localizable", "onboarding_module_subtitle4", fallback: "В каждую тему можно добавить неограниченное количество карточек.")
+  /// Чтобы начать изучать определенную тему свайпните плашку влево.
+  internal static let onboardingModuleSubtitle5 = L10n.tr("Localizable", "onboarding_module_subtitle5", fallback: "Чтобы начать изучать определенную тему свайпните плашку влево.")
+  /// В разделе Повторение показывается какую тему нужно повторить сегодня, а повторение какой темы пропущено.
+  internal static let onboardingModuleSubtitle6 = L10n.tr("Localizable", "onboarding_module_subtitle6", fallback: "В разделе Повторение показывается какую тему нужно повторить сегодня, а повторение какой темы пропущено.")
+  /// В разделе статистика можно посмотреть количество добавленный и выученных карточек за разные периоды времени.
+  internal static let onboardingModuleSubtitle7 = L10n.tr("Localizable", "onboarding_module_subtitle7", fallback: "В разделе статистика можно посмотреть количество добавленный и выученных карточек за разные периоды времени.")
+  /// Добро пожаловать!
+  internal static let onboardingModuleTitle1 = L10n.tr("Localizable", "onboarding_module_title1", fallback: "Добро пожаловать!")
+  /// Добавляйте коллекции!
+  internal static let onboardingModuleTitle2 = L10n.tr("Localizable", "onboarding_module_title2", fallback: "Добавляйте коллекции!")
+  /// Следите за прогрессом!
+  internal static let onboardingModuleTitle3 = L10n.tr("Localizable", "onboarding_module_title3", fallback: "Следите за прогрессом!")
+  /// Добавляйте карточки!
+  internal static let onboardingModuleTitle4 = L10n.tr("Localizable", "onboarding_module_title4", fallback: "Добавляйте карточки!")
+  /// Начать повторение!
+  internal static let onboardingModuleTitle5 = L10n.tr("Localizable", "onboarding_module_title5", fallback: "Начать повторение!")
+  /// Повторяйте!
+  internal static let onboardingModuleTitle6 = L10n.tr("Localizable", "onboarding_module_title6", fallback: "Повторяйте!")
+  /// Статистика!
+  internal static let onboardingModuleTitle7 = L10n.tr("Localizable", "onboarding_module_title7", fallback: "Статистика!")
+  /// Начните изучение коллекции или темы или добавьте карточки в существующие темы
+  internal static let repetitionModuleEmptyCellSubtitle = L10n.tr("Localizable", "repetition_module_empty_cell_subtitle", fallback: "Начните изучение коллекции или темы или добавьте карточки в существующие темы")
+  /// Нет тем для повторения
+  internal static let repetitionModuleEmptyCellTitle = L10n.tr("Localizable", "repetition_module_empty_cell_title", fallback: "Нет тем для повторения")
+  /// Последнее повторение!
+  internal static let repetitionModuleLastRepeat = L10n.tr("Localizable", "repetition_module_last_repeat", fallback: "Последнее повторение!")
+  /// Нужно повторить
+  internal static let repetitionModuleNeedsRepeat = L10n.tr("Localizable", "repetition_module_needs_repeat", fallback: "Нужно повторить")
+  /// Нужно было повторить
+  internal static let repetitionModuleNeedsRepeatPast = L10n.tr("Localizable", "repetition_module_needs_repeat_past", fallback: "Нужно было повторить")
+  /// Повторение
+  internal static let repetitionModuleTitle = L10n.tr("Localizable", "repetition_module_title", fallback: "Повторение")
+  /// Сегодня
+  internal static let repetitionModuleToday = L10n.tr("Localizable", "repetition_module_today", fallback: "Сегодня")
+  /// Завтра
+  internal static let repetitionModuleTomorrow = L10n.tr("Localizable", "repetition_module_tomorrow", fallback: "Завтра")
+  /// Вчера
+  internal static let repetitionModuleYesteday = L10n.tr("Localizable", "repetition_module_yesteday", fallback: "Вчера")
+  /// Повторение
+  internal static let repetitionTab = L10n.tr("Localizable", "repetition_tab", fallback: "Повторение")
+  /// Вы действительно хотите удалить все данные?
+  internal static let settingsModuleDeleteAlertMessage = L10n.tr("Localizable", "settings_module_delete_alert_message", fallback: "Вы действительно хотите удалить все данные?")
+  /// Удаление данных
+  internal static let settingsModuleDeleteAlertTitle = L10n.tr("Localizable", "settings_module_delete_alert_title", fallback: "Удаление данных")
+  /// Удалить все данные
+  internal static let settingsModuleDeleteCellTitle = L10n.tr("Localizable", "settings_module_delete_cell_title", fallback: "Удалить все данные")
+  /// Выбор способа запоминания
+  internal static let settingsModuleLearningCellTitle = L10n.tr("Localizable", "settings_module_learning_cell_title", fallback: "Выбор способа запоминания")
+  /// Показать онбординг
+  internal static let settingsModuleOnboardingCellTitle = L10n.tr("Localizable", "settings_module_onboarding_cell_title", fallback: "Показать онбординг")
+  /// Настройки
+  internal static let settingsModuleTitle = L10n.tr("Localizable", "settings_module_title", fallback: "Настройки")
+  /// Настройки
+  internal static let settingsTab = L10n.tr("Localizable", "settings_tab", fallback: "Настройки")
+  /// Статистика
+  internal static let statisticsTab = L10n.tr("Localizable", "statistics_tab", fallback: "Статистика")
+  /// Добавить
+  internal static let themeModuleAlertAddButton = L10n.tr("Localizable", "theme_module_alert_add_button", fallback: "Добавить")
+  /// Отмена
+  internal static let themeModuleAlertCancelButton = L10n.tr("Localizable", "theme_module_alert_cancel_button", fallback: "Отмена")
+  /// Редактировать
+  internal static let themeModuleAlertEditButton = L10n.tr("Localizable", "theme_module_alert_edit_button", fallback: "Редактировать")
+  /// Отредактируйте название темы
+  internal static let themeModuleAlertEditSubtitle = L10n.tr("Localizable", "theme_module_alert_edit_subtitle", fallback: "Отредактируйте название темы")
+  /// Редактирование темы
+  internal static let themeModuleAlertEditTitle = L10n.tr("Localizable", "theme_module_alert_edit_title", fallback: "Редактирование темы")
+  /// Напишите название темы, которую хотите добавить
+  internal static let themeModuleAlertSubtitle = L10n.tr("Localizable", "theme_module_alert_subtitle", fallback: "Напишите название темы, которую хотите добавить")
+  /// Название темы
+  internal static let themeModuleAlertTextFieldPlaceholder = L10n.tr("Localizable", "theme_module_alert_textField_placeholder", fallback: "Название темы")
+  /// Добавление темы
+  internal static let themeModuleAlertTitle = L10n.tr("Localizable", "theme_module_alert_title", fallback: "Добавление темы")
+  /// Чтобы добавить тему нажите плюс вверху экрана
+  internal static let themeModuleEmptyCellSubtitle = L10n.tr("Localizable", "theme_module_empty_cell_subtitle", fallback: "Чтобы добавить тему нажите плюс вверху экрана")
+  /// Нет тем
+  internal static let themeModuleEmptyCellTitle = L10n.tr("Localizable", "theme_module_empty_cell_title", fallback: "Нет тем")
+  /// Темы
+  internal static let themeModuleHeader = L10n.tr("Localizable", "theme_module_header", fallback: "Темы")
+  /// Добавьте карточки в тему
+  internal static let themeModuleRepeatNoCardsToastTitle = L10n.tr("Localizable", "theme_module_repeat_no_cards_toast_title", fallback: "Добавьте карточки в тему")
+  /// Повторение темы началось
+  internal static let themeModuleRepeatStartedToastTitle = L10n.tr("Localizable", "theme_module_repeat_started_toast_title", fallback: "Повторение темы началось")
+  /// Изучить тему
+  internal static let themeModuleStartLearningTitle = L10n.tr("Localizable", "theme_module_start_learning_title", fallback: "Изучить тему")
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
---
+++ Cofio/Resources/Generated/XCAssets+Generated.swift
@@ -0,0 +1,241 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+#if os(macOS)
+  import AppKit
+#elseif os(iOS)
+  import UIKit
+#elseif os(tvOS) || os(watchOS)
+  import UIKit
+#endif
+#if canImport(SwiftUI)
+  import SwiftUI
+#endif
+
+// Deprecated typealiases
+@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
+internal typealias AssetColorTypeAlias = ColorAsset.Color
+@available(*, deprecated, renamed: "ImageAsset.Image", message: "This typealias will be removed in SwiftGen 7.0")
+internal typealias AssetImageTypeAlias = ImageAsset.Image
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+internal enum Asset {
+  internal static let accentColor = ColorAsset(name: "AccentColor")
+  internal enum Buttons {
+    internal static let pluss = ImageAsset(name: "pluss")
+  }
+  internal enum EmptyCells {
+    internal static let cardEmptyCellIcon = ImageAsset(name: "cardEmptyCellIcon")
+    internal static let mainEmptyCellIcon = ImageAsset(name: "mainEmptyCellIcon")
+    internal static let themeEmptyCellIcon = ImageAsset(name: "themeEmptyCellIcon")
+  }
+  internal enum FinishOffer {
+    internal static let check = ImageAsset(name: "check")
+    internal static let repeated = ImageAsset(name: "repeated")
+  }
+  internal enum Icons {
+    internal static let book = ImageAsset(name: "book")
+    internal static let bulb = ImageAsset(name: "bulb")
+    internal static let calendarNude = ImageAsset(name: "calendar nude")
+    internal static let calendar = ImageAsset(name: "calendar")
+    internal static let classRoom = ImageAsset(name: "class room")
+    internal static let clock = ImageAsset(name: "clock")
+    internal static let cloud = ImageAsset(name: "cloud")
+    internal static let flower = ImageAsset(name: "flower")
+    internal static let food = ImageAsset(name: "food")
+    internal static let heart = ImageAsset(name: "heart")
+    internal static let home = ImageAsset(name: "home")
+    internal static let life = ImageAsset(name: "life")
+    internal static let mail = ImageAsset(name: "mail")
+    internal static let math = ImageAsset(name: "math")
+    internal static let message = ImageAsset(name: "message")
+    internal static let photos = ImageAsset(name: "photos")
+    internal static let pigCoin = ImageAsset(name: "pig coin")
+    internal static let shoe = ImageAsset(name: "shoe")
+    internal static let strava = ImageAsset(name: "strava")
+    internal static let threeGears = ImageAsset(name: "three gears")
+    internal static let train = ImageAsset(name: "train")
+    internal static let twoGears = ImageAsset(name: "two gears")
+    internal static let weight = ImageAsset(name: "weight")
+  }
+  internal enum Onboarding {
+    internal static let onboarding = ImageAsset(name: "onboarding")
+    internal static let onboarding2 = ImageAsset(name: "onboarding2")
+    internal static let onboarding3 = ImageAsset(name: "onboarding3")
+  }
+  internal enum Tabbar {
+    internal static let tab0 = ImageAsset(name: "tab0")
+    internal static let tab1 = ImageAsset(name: "tab1")
+    internal static let tab2 = ImageAsset(name: "tab2")
+    internal static let tab3 = ImageAsset(name: "tab3")
+  }
+  internal enum Toasts {
+    internal static let noThemesToast = ImageAsset(name: "noThemesToast")
+    internal static let repeatToast = ImageAsset(name: "repeatToast")
+  }
+  internal static let iconPlaceholder = ImageAsset(name: "iconPlaceholder")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+internal final class ColorAsset {
+  internal fileprivate(set) var name: String
+
+  #if os(macOS)
+  internal typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Color = UIColor
+  #endif
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  internal private(set) lazy var color: Color = {
+    guard let color = Color(asset: self) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }()
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 11.0, tvOS 11.0, *)
+  internal func color(compatibleWith traitCollection: UITraitCollection) -> Color {
+    let bundle = BundleToken.bundle
+    guard let color = Color(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  internal private(set) lazy var swiftUIColor: SwiftUI.Color = {
+    SwiftUI.Color(asset: self)
+  }()
+  #endif
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
+}
+
+internal extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init?(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+#if canImport(SwiftUI)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+internal extension SwiftUI.Color {
+  init(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle)
+  }
+}
+#endif
+
+internal struct ImageAsset {
+  internal fileprivate(set) var name: String
+
+  #if os(macOS)
+  internal typealias Image = NSImage
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Image = UIImage
+  #endif
+
+  @available(iOS 8.0, tvOS 9.0, watchOS 2.0, macOS 10.7, *)
+  internal var image: Image {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    let name = NSImage.Name(self.name)
+    let image = (bundle == .main) ? NSImage(named: name) : bundle.image(forResource: name)
+    #elseif os(watchOS)
+    let image = Image(named: name)
+    #endif
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 8.0, tvOS 9.0, *)
+  internal func image(compatibleWith traitCollection: UITraitCollection) -> Image {
+    let bundle = BundleToken.bundle
+    guard let result = Image(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  internal var swiftUIImage: SwiftUI.Image {
+    SwiftUI.Image(asset: self)
+  }
+  #endif
+}
+
+internal extension ImageAsset.Image {
+  @available(iOS 8.0, tvOS 9.0, watchOS 2.0, *)
+  @available(macOS, deprecated,
+    message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
+  convenience init?(asset: ImageAsset) {
+    #if os(iOS) || os(tvOS)
+    let bundle = BundleToken.bundle
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSImage.Name(asset.name))
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+#if canImport(SwiftUI)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+internal extension SwiftUI.Image {
+  init(asset: ImageAsset) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle)
+  }
+
+  init(asset: ImageAsset, label: Text) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle, label: label)
+  }
+
+  init(decorative asset: ImageAsset) {
+    let bundle = BundleToken.bundle
+    self.init(decorative: asset.name, bundle: bundle)
+  }
+}
+#endif
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
